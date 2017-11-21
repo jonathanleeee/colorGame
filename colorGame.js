f550ec4d17fe0fde141d3f2c4b1a7ea062d68161
Jonathan Lee <jonathanleeee@gmail.com>
2017-11-20 21:31:49 -0800

    reset button, reset h1 background color

--- colorGame.js
+++ colorGame.js
@@ -1,12 +1,28 @@
 var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickColor = pickedColor();
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
-colorDisplay.textContent = pickColor;
+resetButton.addEventListener("click", function(){
+	//generate all new colors
+	colors = generateRandomColors(6);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match picked color
+	colorDisplay.textContent = pickedColor;
+	//change colors of squares
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323"
+})
+
+
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++){
 	// add initial colors to squares
@@ -17,8 +33,9 @@ for (var i = 0; i < squares.length; i++){
 		// grab color of clicked square
 		var clickedColor = this.style.backgroundColor;
 		// compare color to picked color
-		if(clickedColor === pickColor) {
+		if(clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?"
 			changeColors(clickedColor);
 			h1.style.backgroundColor = clickedColor;
 		} else {
@@ -36,7 +53,7 @@ function changeColors(color){
 	}
 }
 
-function pickedColor(){
+function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
 }
